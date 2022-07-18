5d1af55b14ecd805a383af2ec96df940c43f40d1
pjoon357 <pjoon357@naver.com>
2022-07-18 22:29:09 +0900

    setting

---
+++ src/main/java/com/example/MakeAnything/MakeAnythingApplication.java
@@ -0,0 +1,13 @@
+package com.example.MakeAnything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeAnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeAnythingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MakeAnything/MakeAnythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MakeAnything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeAnythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
