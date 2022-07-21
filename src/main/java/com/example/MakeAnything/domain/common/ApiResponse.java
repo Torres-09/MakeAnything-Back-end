9fb7d3c55abbfea5f16d18e248c615cd95acd37c
pjoon357 <pjoon357@naver.com>
2022-07-21 23:04:42 +0900

    feat: 공통 Response 추가 및 예외 처리 로직 추가

---
+++ src/main/java/com/example/MakeAnything/domain/common/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.example.MakeAnything.common;
+
+import lombok.*;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    private boolean success;
+
+    private T data;
+
+    private ErrorDTO error;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> error(ErrorCode errorCode) {
+        return new ApiResponse<T>(false , null, ErrorDTO.of(errorCode));
+    }
+
+    public static <T> ApiResponse<T> error(ErrorCode errorCode, String message) {
+        return new ApiResponse<T>(false , null, ErrorDTO.of(errorCode, message));
+    }
+}
