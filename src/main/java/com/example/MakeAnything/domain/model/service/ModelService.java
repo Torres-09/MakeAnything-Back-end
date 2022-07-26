c4268b4e2108a85da9612a52da5b1bf44c59eb82
Torres-09 <dla612@naver.com>
2022-07-26 23:43:22 +0900

    Model 작업 중 + 로컬 데이터베이스 설정 추가

--- src/main/java/com/example/MakeAnything/domain/model/service/ModelService.java
+++ src/main/java/com/example/MakeAnything/domain/model/service/ModelService.java
@@ -1,7 +1,12 @@
 package com.example.MakeAnything.domain.model.service;
 
+import com.example.MakeAnything.domain.model.service.dto.GetAllModelsResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface ModelService {
+
+    List<GetAllModelsResponse> getAllModels();
 }
--- src/main/java/com/example/MakeAnything/domain/model/service/ModelServiceImpl.java
+++ src/main/java/com/example/MakeAnything/domain/model/service/ModelServiceImpl.java
@@ -1,4 +1,18 @@
 package com.example.MakeAnything.domain.model.service;
 
-public class ModelServiceImpl {
+import com.example.MakeAnything.domain.model.repository.ModelRepository;
+import com.example.MakeAnything.domain.model.service.dto.GetAllModelsResponse;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ModelServiceImpl implements ModelService{
+
+    private final ModelRepository modelRepository;
+
+    @Override
+    public List<GetAllModelsResponse> getAllModels(){
+        return null;
+    }
 }
