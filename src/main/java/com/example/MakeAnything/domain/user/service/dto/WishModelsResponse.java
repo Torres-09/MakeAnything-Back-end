24b82362c7b2d9db587318cce30aac968cc88440
pjoon357 <pjoon357@naver.com>
2022-07-27 23:04:05 +0900

    feat: wishlist 토글 기능 추가

---
+++ src/main/java/com/example/MakeAnything/domain/user/service/dto/WishModelsResponse.java
@@ -0,0 +1,34 @@
+package com.example.MakeAnything.domain.user.service.dto;
+
+import com.example.MakeAnything.domain.model.model.Model;
+import com.example.MakeAnything.domain.user.model.User;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class WishModelsResponse {
+
+    private Long modelId;
+
+    private String modelName;
+
+    private Long userId;
+
+    private String userName;
+
+    private Long downloadCount;
+
+    private Long price;
+
+    private LocalDateTime createdAt;
+
+    public static WishModelsResponse of(Model model, User user, Long downloadCount) {
+
+        return new WishModelsResponse(model.getId(), model.getModelName(), user.getId(), user.getUserName(),
+                downloadCount, model.getPrice(), model.getCreatedAt());
+    }
+}
---
+++ src/main/java/com/example/MakeAnything/domain/wishlist/model/Wishlist.java
@@ -0,0 +1,34 @@
+package com.example.MakeAnything.domain.wishlist.model;
+
+import com.example.MakeAnything.domain.model.model.Model;
+import com.example.MakeAnything.domain.user.model.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Wishlist {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "model_id")
+    private Model model;
+
+    private Wishlist(User user, Model model) {
+        this.user = user;
+        this.model = model;
+    }
+
+    public static Wishlist of(User user, Model model) {
+        return new Wishlist(user, model);
+    }
+}
