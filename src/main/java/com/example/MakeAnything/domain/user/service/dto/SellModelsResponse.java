b6f225a2d3c9b45664ce83c859ab41322e5c9d30
pjoon357 <pjoon357@naver.com>
2022-07-27 23:04:35 +0900

    feat: wishlist 토글 기능 추가2

--- src/main/java/com/example/MakeAnything/domain/user/service/dto/SellModelsResponse.java
+++ src/main/java/com/example/MakeAnything/domain/user/service/dto/SellModelsResponse.java
@@ -1,14 +1,14 @@
 package com.example.MakeAnything.domain.user.service.dto;
 
 import com.example.MakeAnything.domain.model.model.Model;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SellModelsResponse {
 
     private Long modelId;
@@ -24,16 +24,6 @@ public class SellModelsResponse {
     private Long price;
 
     private LocalDateTime paidAt;
-
-    private SellModelsResponse(Long modelId, String modelName, Long userId, String userName, Long downloadCount, Long price, LocalDateTime paidAt) {
-        this.modelId = modelId;
-        this.modelName = modelName;
-        this.userId = userId;
-        this.userName = userName;
-        this.downloadCount = downloadCount;
-        this.price = price;
-        this.paidAt = paidAt;
-    }
 
     public static SellModelsResponse of(Model model, Long downloadCount) {
 
