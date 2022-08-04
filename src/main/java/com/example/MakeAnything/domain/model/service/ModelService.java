3c70db731ee3dcd0dad3e31b0bfa8db5d1a0012f
Torres-09 <dla612@naver.com>
2022-08-04 17:19:52 +0900

    feat: ModelFile 기능 추가

--- src/main/java/com/example/MakeAnything/domain/model/service/ModelService.java
+++ src/main/java/com/example/MakeAnything/domain/model/service/ModelService.java
@@ -1,5 +1,6 @@
 package com.example.MakeAnything.domain.model.service;
 
+import com.example.MakeAnything.domain.model.model.Model;
 import com.example.MakeAnything.domain.model.service.dto.CreateModelResponse;
 import com.example.MakeAnything.domain.model.service.dto.GetAllModelsResponse;
 import org.springframework.stereotype.Service;
@@ -12,4 +13,6 @@ public interface ModelService {
     List<GetAllModelsResponse> getAllModels();
 
     CreateModelResponse createModel();
+
+    Long save(Model model);
 }
