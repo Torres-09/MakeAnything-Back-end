b1cc858820fe1b2d51997c37c79790770376ba13
imsehwan <dla612@naver.com>
2022-08-10 19:47:13 +0900

    fix: 모델 삭제 기능 수정 ( user validation, optional )

--- src/main/java/com/example/MakeAnything/domain/model/controller/ModelController.java
+++ src/main/java/com/example/MakeAnything/domain/model/controller/ModelController.java
@@ -22,6 +22,7 @@ public class ModelController {
 
     // 전체 모델 조회
     @GetMapping("")
+    @ApiOperation(value = "모델 전체 조회")
     public ApiResponse<List<GetAllModelsResponse>> getAllModels(){
         return ApiResponse.success(modelService.getAllModels());
     }
@@ -29,6 +30,7 @@ public class ModelController {
     // 모델 ( 게시물 ) 생성
     @ResponseBody
     @PostMapping("")
+    @ApiOperation(value = "모델 생성")
     public ApiResponse<CreateModelResponse> createModel(@RequestBody CreateModelRequest createModelRequest) {
 
         Long userId = jwtService.getUserId();
@@ -42,6 +44,7 @@ public class ModelController {
 
     // 모델을 카테고리로 조회
     @GetMapping("/{categoryId}")
+    @ApiOperation(value = "카테고리 조회")
     public ApiResponse<List<GetModelByCategoryResponse>> getModelByCategory(@PathVariable ("categoryId") Long categoryId) {
         return ApiResponse.success(modelService.getModelsByCategory(categoryId));
     }
@@ -49,6 +52,7 @@ public class ModelController {
     // 모델을 태그로 조회 ( 검색 )
     @ResponseBody
     @GetMapping("/search/tag")
+    @ApiOperation(value = "태그로 모델 검색")
     public ApiResponse<GetModelByTagResponse> getModelByTag(@RequestBody GetModelByTagRequest getModelByTagRequest) {
         return null;
     }
@@ -56,18 +60,21 @@ public class ModelController {
     // 모델 이름 조회 ( 검색 )
     @ResponseBody
     @GetMapping("/search/name")
+    @ApiOperation(value = "이름으로 모델 검색")
     public ApiResponse<List<GetModelByNameResponse>> getModelByName(@RequestBody GetModelByNameRequest getModelByNameRequest) {
         return ApiResponse.success(modelService.getModelByName(getModelByNameRequest));
     }
 
     // 인기 모델 상위 조회
     @GetMapping("/topRated")
+    @ApiOperation(value = "다운로드 상위 모델 조회")
     public ApiResponse<List<GetTopModelResponse>> getTopModel() {
         return null;
     }
 
     // 모델 상세 정보 조회
     @GetMapping("/{modelId}")
+    @ApiOperation(value = "모델 상세 정보 조회")
     public ApiResponse<GetModelResponse> getModel(@PathVariable ("modelId") Long modelId){
         return ApiResponse.success(modelService.getModel(modelId));
     }
@@ -75,13 +82,22 @@ public class ModelController {
     // 모델 수정
     @ResponseBody
     @PatchMapping("/{modelId}")
+    @ApiOperation(value = "모델 수정")
     public ApiResponse<UpdateModelResponse> updateModel(@PathVariable ("modelId") Long modelId, @RequestBody UpdateModelRequest updateModelRequest) {
+        Long userId = jwtService.getUserId();
         return ApiResponse.success(modelService.updateModel(modelId, updateModelRequest));
     }
 
     // 모델 삭제
     @DeleteMapping("/{modelId}")
+    @ApiOperation(value = "모델 삭제")
     public ApiResponse<DeleteModelResponse> deleteModel(@PathVariable ("modelId") Long modelId) {
-        return ApiResponse.success(modelService.deleteModel(modelId));
+        Long userId = jwtService.getUserId();
+        DeleteModelResponse deleteModelResponse = modelService.deleteModel(modelId, userId);
+        if (deleteModelResponse.getResultMessage() == "success") {
+            return ApiResponse.success(deleteModelResponse);
+        } else {
+            return ApiResponse.error(ErrorCode.INVALID_AUTH_TOKEN);
+        }
     }
 }
