0641a5e026d93a9439d6920c78494bf369f2739b
Torres-09 <dla612@naver.com>
2022-08-15 23:39:32 +0900

    feat: Download 생성 기능 추가

--- src/main/java/com/example/MakeAnything/domain/download/service/DownloadServiceImpl.java
+++ src/main/java/com/example/MakeAnything/domain/download/service/DownloadServiceImpl.java
@@ -1,10 +1,43 @@
 package com.example.MakeAnything.domain.download.service;
 
+import com.example.MakeAnything.domain.download.model.Download;
+import com.example.MakeAnything.domain.download.repository.DownloadRepository;
+import com.example.MakeAnything.domain.order.model.Order;
+import com.example.MakeAnything.domain.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class DownloadServiceImpl implements DownloadService{
 
+    private final DownloadRepository downloadRepository;
+
+    private final OrderRepository orderRepository;
+
+
+    @Override
+    @Transactional
+    public Long createDownload(Long orderId) {
+
+        Optional<Order> optionalOrder = orderRepository.findById(orderId);
+        Order order = new Order();
+
+        if (optionalOrder.isPresent()) {
+            order = optionalOrder.get();
+        }
+
+
+        Download download = Download.builder()
+                .order(order)
+                .downloadStatus(false)
+                .build();
+
+        downloadRepository.save(download);
+
+        return download.getId();
+    }
 }
